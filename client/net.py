38f2dc5a566038ab93688e1068d82f10fb740239
Vangeance <32186033+Vangeance@users.noreply.github.com>
2020-12-16 05:33:22 -0500

    Update net.py

--- client/net.py
+++ client/net.py
@@ -50,12 +50,12 @@ net = LeNet()
 optimizer = optim.Adam(net.parameters())
 
 if __name__ == "__main__":
-    board_size = 18
+    # set board size below
+    board_size = 10
+    epoch_num = 800
     print(tr.cuda.is_available())
     with open("data%d.pkl" % board_size, "rb") as f:
         (x, y_targ) = pk.load(f)
-
-    # optimizer = tr.optim.Adam(net.parameters())
 
     shuffle = np.random.permutation(range(len(x)))  # shuffle 0-[len(x)-1]
     split = int(0.8 * len(x))
@@ -68,14 +68,14 @@ if __name__ == "__main__":
     print(y_train.shape, y_test.shape)
 
     train_loss, test_loss = [], []
-    for epoch in range(800):
+    for epoch in range(epoch_num):
         y_train, e_train = optimization_step(optimizer, net, x[train], y_targ[train])
         y_test, e_test = calculate_loss(net, x[test], y_targ[test])
         if epoch % 50 == 0: print("%d: %f (%f)" % (epoch, e_train.item(), e_test.item()))
         train_loss.append(e_train.item() / (len(shuffle) - split))
         test_loss.append(e_test.item() / split)
 
-    tr.save(net.state_dict(), "model%d.pth" % board_size)
+    tr.save(net.state_dict(), "../model%d.pth" % board_size)
 
     import matplotlib.pyplot as pt
 
